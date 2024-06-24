dcdc752ba0272dc4fe040d8923551f2b449ae77d
Devon <devon.turple@dal.ca>
2024-06-24 11:56:09 -0300

    added FAQ

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ContactUs from './components/ContactUs';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import FAQ from './components/faq';
 function App() {
 
   return (
@@ -9,7 +10,8 @@ function App() {
       <Header/>
       <div>
         <Routes>
-          <Route path="/" element={<ContactUs />} />
+          {/*<Route path="/" element={<ContactUs />} />*/}
+          <Route path="/" element={<FAQ />} />
         </Routes>
       </div>
       <Footer/>
