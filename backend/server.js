06386e062f684efcac8dde01102a508474cba64b
keyush patel <ky280311@dal.ca>
2024-07-25 21:16:46 -0300

    resources code

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,42 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const cors = require('cors');
+const { getArticles, getVideos } = require('./OnlineResources');
+const { databaseConnection} = require('./db');
 
-const port = process.env.PORT || 3000;
+const app = express();
+const port = 3001;
 
-const server = http.createServer(app);
+app.use(cors());
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.get('/articles', async (req, res) => {
+    try {
+        const articles = await getArticles();
+        res.status(200).json(articles);
+    } catch (err) {
+        res.status(500).send('Error retrieving articles');
+    }
 });
+
+app.get('/videos', async (req, res) => {
+    try {
+        const videos = await getVideos();
+        res.status(200).json(videos);
+    } catch (err) {
+        res.status(500).send('Error retrieving videos');
+    }
+});
+
+databaseConnection()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch(err => {
+        console.error('Error starting server: ', err);
+        process.exit(1);
+    });
+
+
+
+    
